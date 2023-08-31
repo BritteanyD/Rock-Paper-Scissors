2c0275315f46d2591ce1602306787de12bdb4d33
Britteany <miszbritteany@gmail.com>
2023-08-30 22:13:20 -0400

    Called function, fixed syntax errors

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 
 console.log("Welcome to Britteany's Rock Paper Scissors Game!");
 
-function getComputerChoice(){
+function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
-    return options[Math.floor(Math.random()*3)];
+    return options[Math.floor(Math.random() * 3)];
 }
 let computerChoice = getComputerChoice()
-console.log(computerChoice)
+console.log("computerChoice", computerChoice)
 
-var playerChoice = prompt("Choose rock, paper, or scissors");
+var playerChoice = prompt("Choose rock, paper, or scissors")
+    .toString()
+    .toLowerCase();
 
+console.log("playerChoice", playerChoice);
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice)
+        alert("It's a tie!")
+    else if (playerChoice === 'rock' && computerChoice === 'paper')
+        alert("Paper beats rock, you lose")
+    else if (playerChoice === 'scissors' && computerChoice === 'rock')
+        alert("Rock beats scissors, you lose")
+    else if (playerChoice === 'paper' && computerChoice === 'scissors')
+        alert("Scissors beat paper, you lose")
+    else if (playerChoice === 'paper' && computerChoice === 'rock')
+        alert("You win! Paper beats rock")
+    else if (playerChoice === 'rock' && computerChoice === 'scissors')
+        alert("You win! Rock beats scissors")
+    else if (playerChoice === 'scissors' && computerChoice === 'paper')
+        alert("You win! Scissors beats paper")
+}
 
-console.log(playerChoice.toString().toLowerCase());
-function playRound((playerChoice.toString().toLowerCase(), computerChoice)){
-if (playerChoice.toString().toLowerCase() === computerChoice)
-    alert("It's a tie!") 
-else if (playerChoice.toString().toLowerCase() === 'rock' && computerChoice === 'paper')  
-    alert("Paper beats rock, you lose") 
-else if (playerChoice.toString().toLowerCase() === 'scissors' && computerChoice === 'rock')  
-    alert("Rock beats scissors, you lose")  
-else if (playerChoice.toString().toLowerCase() === 'paper' && computerChoice === 'scissors')  
-    alert("Scissors beat paper, you lose")   
-else if (playerChoice.toString().toLowerCase() === 'paper' && computerChoice === 'rock')  
-    alert("You win! Paper beats rock")   
-else if (playerChoice.toString().toLowerCase() === 'rock' && computerChoice === 'scissors')  
-    alert("You win! Rock beats scissors")  
-else if (playerChoice.toString().toLowerCase() === 'scissors' && computerChoice === 'paper')  
-    alert("You win! Scissors beats paper")   
-    } 
+playRound(playerChoice, computerChoice);
 
 
